0bb042f2817f857f74b07ad57de7358ee615ec47
Jessica Temporal <jessica.temporal@auth0.com>
2021-08-19 14:30:00 -0300

    Adds protected endpoint

--- application/main.py
+++ application/main.py
@@ -1,7 +1,14 @@
-"""FastAPI starter sample
+"""FastAPI starter sample: Protected endpoint
 """
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Response, status
+from fastapi.security import HTTPBearer
+
+from .utils import VerifyToken
+
+
+# Scheme for the Authorization header
+token_auth_scheme = HTTPBearer()
 
 app = FastAPI()
 
@@ -19,14 +26,13 @@ def public():
 
 
 @app.get("/api/private")
-def private():
+def private(response: Response, token: str = Depends(token_auth_scheme)):
     """A valid access token is required to access this route"""
 
-    # We need to protect this endpoint!
+    result = VerifyToken(token.credentials).verify()
 
-    result = {
-        "status": "success",
-        "msg": ("Hello from a private endpoint! "
-                "You need to be authenticated to see this.")
-    }
+    if result.get("status"):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return result
+
     return result
